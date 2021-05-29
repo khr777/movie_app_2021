eccf56f0fcd4d0c3aae8a008c3068a685ddac859
khr777 <kim5638yw@gmail.com>
2021-05-29 10:15:56 +0900

    �리액트 기초 개념 : props, props 구조분해할당

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-import Potato from './Potato';
+
+
+function Food(props) {
+  console.log(props);
+  return <h1>I like { props.fav }</h1>
+}
+
+function Season({ season }) {
+  return <h2>I love { season }</h2>
+}
 
 function App() {
   return <div>
       <h1>HELLO WORLD!!</h1>
-      <Potato />
+      <Food fav="kimchi" something={true} papapa={[1, 2, 3, 4, true]} />
+      <Season season="spring"/>
+      <Season season="summer"/>
+      <Season season="fall"/>
+      <Season season="winter"/>
         </div>
 }
 
