939adb464e1f48759ace9ed76a5c334fde7dc187
khr777 <kim5638yw@gmail.com>
2021-05-30 15:44:41 +0900

    state 정의하고 button onClick 이벤트 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  state = {
+    count: 0
+  }
+
+  add = () => {
+    console.log('add');
+  };
+
+  minus = () => {
+    console.log('minus');
+  }
+
   render() {
-    return <h1>I'm a class Component.</h1>
+    return <div>
+      <h1>This number : {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>
   }
 }
 
